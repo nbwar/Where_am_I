87d2303d618eeaa8b2ef014eaa8bd6c1be2d32b6
Nicholas Wargnier <nwargnier@gmail.com>
2013-11-13 19:31:58 -0800

    initial commit

---
+++ where_am_i_api/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+WhereAmI::Application.config.secret_key_base = 'ae38d92998abde550fb6166528c3a50f7d6e692ab159e31b752d691dc91b86e783b6759d0d04d5ac06c51a95768a81586bb85771c8942b29d2769e800e2974d8'
---
+++ where_am_i_api/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+WhereAmI::Application.config.session_store :cookie_store, key: '_where_am_i_session'
