1750bd763a7e2d03de346eed95842a00ce2ad8a3
Nicholas Wargnier <nwargnier@gmail.com>
2013-11-15 13:26:51 -0800

    Distance calucluated using geocoder

--- where_am_i_api/app/controllers/api/location_controller.rb
+++ where_am_i_api/app/controllers/api/location_controller.rb
@@ -1,12 +1,10 @@
 module Api
   class LocationController < ApplicationController
     def determine_distance
-      # Geocoder.coordinates(params[""])
-      lat = params["latitude"]
-      lng = params["longitude"]
-      location = params["location"]
-
-      render :json => {blah: 'blah'}
+      location = Geocoder.coordinates(params["location"])
+      current_location = [params["latitude"].to_f, params["longitude"].to_f]
+      distance = Geocoder::Calculations.distance_between(current_location, location)
+      render :json => {distance: distance}
     end
   end
 end
